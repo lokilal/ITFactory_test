7f9eb624ce9cf1a7dc1c6d5c055fcbe65ae3d9ab
Кирилл <k_kirill45@mail.ru>
2022-05-09 11:32:55 +0300

    Create models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 
+class Worker(models.Model):
+    name = models.CharField(
+        max_length=255, verbose_name='Имя'
+    )
+    phone_number = models.CharField(
+        max_length=255, verbose_name='Номер телефона'
+    )
+
+
+class SalesPoint(models.Model):
+    name = models.CharField(
+        max_length=255, verbose_name='Название'
+    )
+    worker = models.ForeignKey(
+        Worker, on_delete=models.CASCADE, verbose_name='Работник'
+    )
+
+
+class Visiting(models.Model):
+    date = models.DateTimeField(
+        verbose_name='Дата/время'
+    )
+    sales_point = models.ForeignKey(
+        SalesPoint, on_delete=models.CASCADE, verbose_name='Торговая точка'
+    )
+    latitude = models.FloatField(
+        verbose_name='Широта'
+    )
+    longitude = models.FloatField(
+        verbose_name='Долгота'
+    )
